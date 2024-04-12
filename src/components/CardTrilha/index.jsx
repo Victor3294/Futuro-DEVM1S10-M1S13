fb3d45baf41226ef08565e5356b6c7412a886ab7
Victor3294 <vblum26@gmail.com>
2024-04-12 18:36:29 -0300

    proptypes semana10

--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,12 +1,27 @@
-
+import * as PropTypes from "prop-types"
 
 function CardTrilha ({dadosTrilha}){
     return(
         <>
             <h1>{dadosTrilha.nomeTrilha}</h1>
             <span>{dadosTrilha.cidade}/{dadosTrilha.estado}</span>
-            <img src={dadosTrilha.urlImagem} alt="" />
+            <img width={200} src={dadosTrilha.urlImagem} alt="imagem trilha" />
         </>
     )
+}
+
+CardTrilha.propTypes = {
+    dadosTrilha: PropTypes.exact({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string.isRequired,
+        estado: PropTypes.string.isRequired,
+        duracao: PropTypes.number.isRequired,
+        trajeto: PropTypes.number.isRequired,
+        dificuldade: PropTypes.string.isRequired,
+        // enum
+        tipo: PropTypes.oneOf(['caminhada / trekking', 'ciclismo']),
+        nomeUsuario: PropTypes.string.isRequired,
+        urlImagem: PropTypes.string.isRequired
+    })
 }
 export default CardTrilha;
