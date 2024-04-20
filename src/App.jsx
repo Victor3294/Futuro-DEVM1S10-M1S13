e4fbd5920c9fd35bf80bd6e7c6f6cefb145e473c
Victor3294 <vblum26@gmail.com>
2024-04-19 23:30:05 -0300

    feat: 🎸 Relocação da lista Trilhas
    Mundança do locla do codigo de listagem de trilhas do App.jsx para a
    PaginaListaTrilhas.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,14 @@
-import CardTrilha from "./components/CardTrilha"
-import useFetch from "./hooks/useFetch.jsx"
-import Cabecalho from "./components/Cabecalho/index.jsx"
-import "./App.css"
-import { useEffect, useState } from "react"
-import { Outlet } from "react-router-dom"
+import Cabecalho from "./components/Cabecalho/index.jsx";
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [listaDeTrilhas, isLoading] = useFetch(
-    "/listaTrilhas.json"
-  )
-
-  const [trilhas, setTrilhas] = useState([])
-  useEffect(() =>{
-    if(!!listaDeTrilhas && !isLoading){
-      setTrilhas(listaDeTrilhas.trilhas)
-    }
-  }, [listaDeTrilhas])
-
   return (
     <>
-    <Cabecalho />
-    <Outlet />
-
-
-
-
-   
-      
-      
+      <Cabecalho />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
-
-//  {isLoading && <p>Trilhas carregando</p>}
-//     {!isLoading && !!trilhas &&
-//      trilhas.map((trilha, index) => (
-//         <CardTrilha dadosTrilha={trilha} key={index} />
-//       ))
-//     }
+export default App;
--- src/pages/PaginaListaTrilhas.jsx
+++ src/pages/PaginaListaTrilhas.jsx
@@ -1,10 +1,27 @@
-function PaginaListaTrilhas(){
-    return(
-        <>
-            <h1>Pagina Lista trilhas</h1>
-        </>
-    )
+import { useEffect, useState } from "react";
+import CardTrilha from "../components/CardTrilha";
+import useFetch from "../hooks/useFetch";
+function PaginaListaTrilhas() {
+  const [listaDeTrilhas, isLoading] = useFetch("/listaTrilhas.json");
 
+  const [trilhas, setTrilhas] = useState([]);
+  useEffect(() => {
+    if (!!listaDeTrilhas && !isLoading) {
+      setTrilhas(listaDeTrilhas.trilhas);
+    }
+  }, [listaDeTrilhas]);
+
+  return (
+    <>
+      <h1>Pagina Lista trilhas</h1>
+      {isLoading && <p>Trilhas carregando</p>}
+      {!isLoading &&
+        !!trilhas &&
+        trilhas.map((trilha, index) => (
+          <CardTrilha dadosTrilha={trilha} key={index} />
+        ))}
+    </>
+  );
 }
 
-export default PaginaListaTrilhas
+export default PaginaListaTrilhas;
